5a6f46f1f5af66bcae51b18e6769edb52102f95a
AnaLuiza <nininha_alesilva@hotmail.com>
2025-07-08 14:03:50 -0300

    Iniciando o setup

---
+++ ARQUIVOS/github_finder/src/routes/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Search from '../components/Search';
+
+import { useState } from 'react';
+
+const Home = () => {
+   const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default Home;
